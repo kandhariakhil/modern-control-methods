23754a63107acdb326f77644daa7722302d3cf75
kandhariakhil <axk751@case.edu>
2023-09-18 07:54:50 -0400

    WIP: Dynamic model returns NaN

--- Model Predictive Control/Autonomous Driving Dynamic MPC/MPC.py
+++ Model Predictive Control/Autonomous Driving Dynamic MPC/MPC.py
@@ -8,6 +8,23 @@ import matplotlib.pyplot as plt
 
 from importParameters import importParameters
 
+from draw_sim import simulate
+
+def shift_timestep(step_horizon, t0, state_init, u, f):
+    f_value = f(state_init, u[:, 0])
+    next_state = ca.DM.full(state_init + (step_horizon * f_value))
+
+    t0 = t0 + step_horizon
+    u0 = ca.horzcat(
+        u[:, 1:],
+        ca.reshape(u[:, -1], -1, 1)
+    )
+
+    return t0, next_state, u0
+
+def DM2Arr(dm):
+    return np.array(dm.full())
+
 def calFrontForce(lf, B, C, D, omega, vx, vy, delta):
     alpha = -ca.arctan2(omega * lf + vy, vx) + delta
     frontForce = D * ca.sin(C * ca.arctan(B * alpha))
@@ -20,15 +37,17 @@ def calRearForce(lr, B, C, D, omega, vx, vy):
     
     return rearForce
 
-def calLongitudinalForce():
-    pass
+def calLongForce(cm1,cm2,cr,cd,vx,d):
+    longForce = (cm1-cm2*vx)*d-cr-cd*vx*vx
+    
+    return longForce
 
 x_init = 0.0
 y_init = 0.0
 phi_init = 0.0
 vx_init = 0.0
 vy_init = 0.0
-omega = 0.0
+omega_init = 0.0
 
 x_target = 30.0
 y_target = 30.0
@@ -40,7 +59,7 @@ omega_target = 0.0
 parameters = importParameters()
 
 dt = 0.25
-N = 10
+N = 3
 sim_time = 100
 
 mp = parameters.getModelParameters()
@@ -79,3 +98,218 @@ front_force_func = ca.Function('front_force_func', [omega, vx, vy, delta], [fron
 
 rear_force = calRearForce(mp['lf'], mp['Bf'], mp['Cf'], mp['Df'], omega, vx, vy)
 rear_force_func = ca.Function('rear_force_func', [omega, vx, vy], [rear_force])
+
+long_force = calLongForce(mp['cm1'],mp['cm2'],mp['cr'],mp['cd'],vx,d)
+long_force_func = ca.Function('long_force',[vx,d],[long_force])
+print(mp['Iz'])
+rhs = ca.vertcat(
+    vx*ca.cos(phi)-vy*ca.sin(phi),
+    vx*ca.sin(phi)+vy*ca.cos(phi),
+    omega,
+    (1/mp['m'])*(long_force_func(vx,d)-front_force_func(omega,vx,vy,delta)*ca.sin(delta)+mp['m']*vy*omega),
+    (1/mp['m'])*(rear_force_func(omega,vx,vy)-front_force_func(omega,vx,vy,delta)*ca.cos(delta)-mp['m']*vx*omega),
+    (1/mp['Iz'])*(front_force_func(omega,vx,vy,delta)*mp['lf']*ca.cos(delta)-rear_force_func(omega,vx,vy)*mp['lr'])
+)
+
+f = ca.Function('f',[states,controls],[rhs])
+
+U = ca.SX.sym('U',n_controls,N)
+
+P = ca.SX.sym('P',2*n_states)
+
+X = ca.SX.sym('X',n_states,N+1)
+
+obj = 0
+g = X[:,0]-P[:n_states]
+
+Q11 = 10.0
+Q22 = 10.0
+Q33 = 5.0
+Q44 = 5.0
+Q55 = 2.0
+Q66 = 2.0
+
+Q = ca.diagcat(Q11,Q22,Q33,Q44,Q55,Q66)
+
+R11 = 0.5
+R22 = 2.0
+
+R = ca.diagcat(R11,R22)
+
+S11 = 1
+S22 = 0.05
+
+S = ca.diagcat(S11,S22)
+
+for k in range(N):
+    st = X[:,k]
+    con = U[:,k]
+
+    obj += ca.mtimes([(st-P[n_states:]).T,Q,(st-P[n_states:])]) + ca.mtimes([con.T,R,con])
+    k1 = f(st,con)
+    k2 = f(st+((dt/2)*k1),con)
+    k3 = f(st+((dt/2)*k2),con)
+    k4 = f(st+(dt*k3),con)
+    st_next = X[:, k+1]
+    st_next_rk45 = st+(dt/6)*(k1+2*k2+2*k3+k4)
+    g = ca.vertcat(g,st_next-st_next_rk45)
+
+    if k > 0:
+        prev_con = U[:,k-1]
+        control_rate = con-prev_con
+        obj += ca.mtimes([control_rate.T,S,control_rate])
+
+opt_variables = ca.vertcat(X.reshape((-1,1)),
+                            U.reshape((-1,1)))
+
+nlp_prob = {
+    'f':obj,
+    'x':opt_variables,
+    'g':g,
+    'p':P
+}
+
+opts = {
+    'ipopt': {
+        'max_iter':100,
+        'print_level':0,
+        'acceptable_tol':1e-8,
+        'acceptable_obj_change_tol':1e-6
+    },
+    'print_time':0
+}
+
+solver = ca.nlpsol('solver','ipopt',nlp_prob,opts)
+
+args = {
+    'lbg' : np.zeros(n_states*(N+1)), # Lower bound of states x and y
+    'ubg' : np.zeros(n_states*(N+1)), # Upper bound of states x and y 
+    # Input constraints
+    'lbx' : np.zeros(n_states*(N+1)+(n_controls*N)),
+    'ubx' : np.zeros(n_states*(N+1)+(n_controls*N))
+}
+
+for i in range(0,n_states*(N+1)-(n_states-1),n_states):
+    args['lbx'][i] = lim['x_min']
+    args['lbx'][i+1] = lim['y_min']
+    args['lbx'][i+2] = lim['phi_min']
+    args['lbx'][i+3] = lim['vx_min']
+    args['lbx'][i+4] = lim['vy_min']
+    args['lbx'][i+5] = lim['omega_min']
+
+    args['ubx'][i] = lim['x_max']
+    args['ubx'][i+1] = lim['y_max']
+    args['ubx'][i+2] = lim['phi_max']
+    args['ubx'][i+3] = lim['vx_max']
+    args['ubx'][i+4] = lim['vy_max']
+    args['ubx'][i+5] = lim['omega_max']
+
+for i in range(n_states*(N+1),len(args['lbx'])-(n_controls-1),n_controls):
+    args['lbx'][i] = lim['d_min']
+    args['lbx'][i+1] = lim['delta_min']
+
+    args['ubx'][i] = lim['d_max']
+    args['ubx'][i+1] = lim['delta_max']
+
+# Simulation Loop
+t0 = 0
+
+#DM is a dense matrix which is a 2D array with arbitrary dimensions and a fixed data type
+x0 = ca.DM([x_init,y_init,phi_init,vx_init,vy_init,omega_init])
+xs = ca.DM([x_target, y_target, phi_target,vx_target,vy_target,omega_target])
+
+t = ca.DM(t0)
+u0 = ca.DM.zeros((n_controls,N)) #Initial control - Is Nxn_controls in video lecture
+X0 = ca.repmat(x0,1,N+1) #Initial state full
+
+#Start MPC
+mpc_iter = 0
+cat_states = DM2Arr(X0) #Store predicted state 
+cat_controls = DM2Arr(u0[:,0]) # Store control actions
+state_outputs = DM2Arr(x0)
+times = np.array([[0]])
+
+
+if __name__ == '__main__':
+    main_loop = time.time()
+    while (ca.norm_2(x0-xs) > 1e-1) and mpc_iter < sim_time/dt:
+        t1 = time.time()
+        args['p'] = ca.vertcat(
+            x0,    # current state
+            xs     # target state
+            )
+        # optimization variable current state as a 1D vector
+        args['x0'] = ca.vertcat(
+            ca.reshape(X0, n_states*(N+1),1),
+            ca.reshape(u0,n_controls*N,1))
+
+        sol = solver(x0=args['x0'], 
+                    lbx = args['lbx'], 
+                    ubx = args['ubx'], 
+                    lbg = args['lbg'], 
+                    ubg = args['ubg'], 
+                    p= args['p'])
+        
+        # Extract the control input
+        u = ca.reshape(sol['x'][n_states * (N + 1):], n_controls, N)
+        X0 = ca.reshape(sol['x'][: n_states * (N+1)], n_states, N+1)
+        # Compute optimal solution trajectory
+        # Compute state given new control input
+
+        cat_controls = np.vstack((
+            cat_controls,
+            DM2Arr(u[:, 0])
+        ))
+
+        cat_states = np.dstack((
+            cat_states,
+            DM2Arr(X0)
+        ))
+
+        t = np.vstack((
+            t,
+            t0
+        ))
+
+        state_outputs = np.vstack((
+            state_outputs,
+            (x0)
+        ))
+
+        t0, x0, u0 = shift_timestep(dt, t0, x0, u, f)
+
+        # print(X0)
+        X0 = ca.horzcat(
+            X0[:, 1:],
+            ca.reshape(X0[:, -1], -1, 1)
+        )
+
+        # xx ...
+        t2 = time.time()
+
+        times = np.vstack((
+            times,
+            t2-t1
+        ))
+
+        mpc_iter += 1
+
+    main_loop_time = time.time()
+    ss_error = ca.norm_2(x0 - xs)
+    control_actions = (ca.reshape(cat_controls,n_controls,-1))
+    vehicle_state = (ca.reshape(state_outputs,n_states,-1))
+
+    fig,ax = plt.subplots(2,1)
+    ax[0].plot(t,control_actions[0,:].T)
+    ax[0].set_title('Acceleration vs Time')
+    ax[1].plot(t,control_actions[1,:].T)
+    ax[1].set_title('Steering Angle vs Time')
+
+    plt.show
+    print('\n\n')
+    print('Total time: ', main_loop_time - main_loop)
+    print('avg iteration time: ', np.array(times).mean() * 1000, 'ms')
+    print('final error: ', ss_error)
+    # simulate
+    simulate(cat_states, cat_controls, times, dt, N,
+            np.array([x_init, y_init, phi_init, x_target, y_target, phi_target]), save=False)
